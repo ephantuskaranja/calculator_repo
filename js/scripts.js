870081b7a4b75d8dd8daf5d2d9b388cb4e33f1a7
Ephantus karanja <ephantusslogan@gmail.com>
2018-04-24 16:43:30 +0300

    add bmi function to calculator

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,17 @@ var add = function(number1, number2) {
      var number2 = parseInt(prompt("Enter another number:"));
      var result = add(number1, number2);
      alert(result);
+var subtract=function(num1,num2){
+  return num1-num2;
+};
+var num1=parseInt(prompt("enter first number:"));
+var num2=parseInt(prompt("Enter second number"));
+var result=subtract(num1, num2);
+alert(result);
+var bmi=function(w,h){
+  return w/(h*h);
+};
+var w=parseFloat(prompt("enter weight:"));
+var h=parseFloat(prompt("Enter height"));
+var result=bmi(w, h);
+alert(result);
