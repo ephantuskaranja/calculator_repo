92979df829fd174891be4e5e9fed7b857ff47106
Ephantus karanja <ephantusslogan@gmail.com>
2018-04-25 08:16:55 +0300

    add gallons to litres conversion function

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-//add function
+/*//add function
 var add = function(number1, number2) {
        return number1 + number2;
      };
@@ -22,6 +22,22 @@ var bmi=function(w,h){
   return w/(h*h);
 };
 var w=parseFloat(prompt("enter weight:"));
-var h=parseFloat(prompt("Enter height"));
+var h=parseFloat(5prompt("Enter height"));
 var result=bmi(w, h);
 alert(result);
+
+//temperature to fareheight conversion
+var tempConvert=function(c){
+  return c*1.8+32;
+};
+var c=parseFloat(prompt("enter degree in celcius:"));
+var answer=tempConvert(c);
+alert(answer);*/
+
+//gallons to litres conversion function
+var gl = function(g) {
+  return g*4.5461;
+};
+var g=parseFloat(prompt("enter amount in litres:"));
+var answer=gl(g);
+alert(answer);
